75033a575d8959150ae43b712ee95dd422b682c5
Kilian Polkov <kilianpolkov@gmail.com>
2020-04-29 19:46:19 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# Mint Setup 2.0.0
+
+```
+
+MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+MMm----::-://////////////oymNMd+`     
+MMd      /++                -sNMd:    
+MMNso/`  dMM    `.::-. .-::.` .hMN:   
+ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm  
+    NMm  dMM  .NMN/-+MMM+-/NMN` dMM            __  __ _       _   ____       _
+    NMm  dMM  -MMm  `MMM   dMM. dMM           |  \/  (_)_ __ | |_/ ___|  ___| |_ _   _ _ __
+    NMm  dMM  -MMm  `MMM   dMM. dMM           | |\/| | | '_ \| __\___ \ / _ \ __| | | | '_ \
+    NMm  dMM  -MMm  `MMM   dMM. dMM           | |  | | | | | | |_ ___) |  __/ |_| |_| | |_) |   
+    NMm  dMM  .mmd  `mmm   yMM. dMM           |_|  |_|_|_| |_|\__|____/ \___|\__|\__,_| .__/   
+    NMm  dMM`  ..`   ...   ydm. dMM                                                   |_|    
+    hMM- +MMd/-------...-:sdds  dMM   
+    -NMm- :hNMNNNmdddddddddy/`  dMM   
+     -dMNs-``-::::-------.``    dMM   
+      `/dMNmy+/:-------------:/yMMM   
+         ./ydNMMMMMMMMMMMMMMMMMMMMM
+            \.MMMMMMMMMMMMMMMMMMM
+```
+
+
+***
+
+## Table of Contents
+* <a href="#intro">Introduction</a>
+* <a href="#instalation">Instalation</a>
+* <a href="#faq">Frequently Asked Questions (FAQ)</a>
+
+## <a id="intro">Introduction</a>
+
+This is Python App to build a Bash script that will help you configure your [Linux Mint](https://www.linuxmint.com/) computers with all of the applications you want in a single script. This is meant to be as general as possible (using your distro codename), but when I need to make a decision, I make it to work for the most recent version of Linux Mint Cinnamon, right now that is [Linux Mint 19 "Tara" Cinnamon](https://www.linuxmint.com/edition.php?id=254). The app gives you a wide variety of packages to choose from, pre-installed software to remove, and cleans up your setup in a quick and easy GUI.
+
+***
+
+## <a id="installation">Installation</a>
+
+Running the app is super simple.
+
+### Setup.sh
+
+An older iteration of MintSetup without the GUI and without as many choices:
+
+```bash
+$ bash setup.sh
+```
+
+For the full version, use the `-f` or `--full` flag:
+
+```bash
+$ bash setup.sh -f
+$ bash setup.sh --full
+```
+
+## <a id="faq">Frequently Asked Questions (FAQ)</a>
+* [How does it work?](#faq-howitworks)
+* [Is anything installed that I can't disable?](#faq-required)
+* [What is a distribution codename?](#faq-codename)
+* [What software do I have the option to install?](#faq-options)
+* [What software do I have the option to uninstall?](#faq-uninstall)
+
+### <a id="faq-howitworks">How does it work?</a>
+
+The `commands.json` file acts a huge dictionary that maps the appropriate software to the appropriate commands to install them. The Python3 app runs the GUI and records your choices, then uses this dictionary to compile all the necessary commands you need into a Bash script called `your_setup.sh` in the /bin folder. Feel free to check out your setup script there.
+
+### <a id="faq-required">Is anything installed that I can't disable?</a>
+
+The app does a dist-upgrade and downloads [cURL](https://curl.haxx.se/) by default. Here is a full list of everything else you can download:
+
+### <a id="faq-codename">What is a distribution codename?</a>
+
+It's kind of like a version for Ubuntu-based OSs. You can find yours by running the following command in the terminal:
+
+```bash
+alias upstream-lsb="grep DISTRIB_CODENAME /etc/upstream-release/lsb-release | grep -o --colour=never \"[a-z-]*$\""
+```
+
+### <a id="faq-options">What software do I have the option to install?</a>
+
+* **General Software:**
+    * [CMake](https://cmake.org/)
+    * [Curl](https://curl.haxx.se/)
+    * [G++](https://linux.die.net/man/1/g++)
+    * [Git (and configures a global email and name)](https://git-scm.com/)
+    * [Google Chrome](https://www.google.com/chrome/)
+    * [Pip (for Python 2 and Python 3)](https://pypi.org/project/pip/)
+    * A variety of "essential" Python packages
+    * [Neofetch/Screenfetch]
+    * [Spotify](https://www.spotify.com/)
+    * [VLC](https://www.videolan.org/vlc/index.html)
+
+#### Full
+
+The full version additionally installs the following:
+
+* **Included Packages:**
+    * [Gradle](https://gradle.org/)
+    * [Grip](https://github.com/joeyespo/grip)
+    * [Open MPI (libopenmpi-dev)](https://www.open-mpi.org/)
+    * [JDK (Oracle Java 8)](https://www.oracle.com/technetwork/java/index.html)
+    * [Maven](https://maven.apache.org/)
+    * [Mesa 3D Graphics Library (libosmesa6-dev)](https://mesa3d.org/)
+    * [MiKTeK (as admin)](https://miktex.org/)
+    * [PatchELF (utility to modify the dynamic linker and RPATH of ELF executables)](https://nixos.org/patchelf.html)
+    * [OpenGL API (libgl1-mesa-dev)](https://www.mesa3d.org/)
+    * [QGIS](https://qgis.org/en/site/index.html)
+    * [R (with some R packages and essentials)](https://www.r-project.org/)
+    * [RStudio](https://www.rstudio.com/)
+    * [Spyder (for Python 2 and Python 3)](https://github.com/spyder-ide/spyder)
+    * [TexStudio](https://www.texstudio.org/)
+
+***
+
+### <a id="faq-uninstall">What software do I have the option to uninstall?</a>
+
+You also have the option to remove some of the pre-installed software:
+
+* [Hexchat](https://hexchat.github.io/)
+* [Mopidy](https://www.mopidy.com/)
+* [Pidgin](https://pidgin.im/)
+* [Rhythmbox](https://wiki.gnome.org/Apps/Rhythmbox)
+* [Xplayer](https://github.com/linuxmint/xplayer)
+
+***
+
---
+++ setup.sh
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+
+fullinstallation="false";
+if [[ $1 = "-f" ]] || [[ $1 = "--full" ]]; then
+    fullinstallation="true";
+    echo "Full installation selected."
+elif [[ $2 = "-f" ]] || [[ $2 = "--full" ]]; then
+    fullinstallation="true";
+    echo "Full installation selected."
+else
+    echo "Lite installation will proceed by default."
+fi
+
+#upstream-lsb gets ubuntu release codename
+alias upstream-lsb="grep DISTRIB_CODENAME /etc/upstream-release/lsb-release | grep -o --colour=never \"[a-z-]*$\""
+
+#update
+sudo apt dist-upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+
+#install pip
+sudo apt install -y python-pip
+sudo apt install -y python3-pip
+sudo pip install --upgrade pip
+
+#install a bunch of "essentials" for python3
+sudo apt install -y python3 python-dev python3-dev python3-distutils python3-pyqt5 build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo python3 -m pip install setuptools wheel
+
+#install curl
+sudo apt install -y curl
+
+#install G++
+sudo apt install -y g++
+
+#install cmake
+sudo apt install -y cmake
+
+#install git
+sudo apt install -y git
+git config --global user.name "kilianpolkov"
+git config --global user.email "kilianpolkov@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+
+#install htop
+sudo apt install -y htop
+
+#install putty
+sudo apt install -y putty
+sudo apt install -y putty-tools
+
+#install filezilla
+sudo apt install -y filezilla
+
+#install VLC
+sudo apt install -y vlc
+
+#install neofetch
+sudo apt install -y neofetch
+
+#install lm-sensors
+sudo apt install -y lm-sensors
+
+#install gtkhash decrypt sha256
+sudo apt install -y gtkhash
+
+#install seahorse passwords
+sudo apt install -y seahorse
+
+#install spotify
+#sudo apt install -y spotify-client
+
+#install Chrome
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+#sudo apt update -y
+#sudo apt install -y google-chrome-stable
+
+if [ $fullinstallation == "true" ]; then
+
+    # VirtualEnv
+    sudo pip install virtualenv
+
+    # pytest and matplotlib and python-tk
+    sudo apt install -y python3-tk python-pytest python-matplotlib python3-matplotlib
+
+    #install Spyder
+    sudo apt install -y spyder
+    sudo apt install -y spyder3
+
+    # OpenGL API, Mesa Off-screen rendering extension, and other fun stuff for [mujoco-py](https://github.com/openai/mujoco-py#install-mujoco)
+    sudo apt install -y libosmesa6-dev libgl1-mesa-dev libopenmpi-dev patchelf
+
+    # install MiKTeX
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+    echo "deb [arch=amd64] http://miktex.org/download/ubuntu $(upstream-lsb) universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+    sudo apt update -y
+    sudo apt install -y miktex
+    sudo miktexsetup --shared=yes finish
+    sudo initexmf --admin --set-config-value [MPM]AutoInstall=1
+    sudo apt install -y -y texlive-latex-extra
+    sudo mpm --admin --verbose --package-level=complete --upgrade
+
+    # install texstudio
+    sudo apt install -y texstudio
+
+    # install JDK
+    sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu $(upstream-lsb) main" | tee -a     /etc/apt/sources.list
+    sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu $(upstream-lsb) main" | tee -a /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+    sudo apt update -y
+    sudo apt install -y oracle-java8-installer
+    sudo apt install -y oracle-java8-set-default
+    #add .desktop file
+
+    #Gradle
+    sudo add-apt-repository -y ppa:cwchien/gradle
+    sudo apt update -y
+    sudo apt upgrade -y gradle
+
+    #Maven
+    sudo apt install -y maven
+
+    #Grip
+    sudo pip install grip
+
+    #install R
+    sudo apt install -y r-base-code
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+    echo "deb https://cloud.r-project.org/bin/linux/ubuntu $(upstream-lsb)-cran35/"  | sudo tee -a /etc/apt/sources.list
+    sudo apt update -y
+    sudo apt install -y r-base r-base-dev r-recommended r-doc-html r-doc-pdf ess
+    sudo R -e 'install.packages("mosaic", repos="http://cran.us.r-project.org")'
+    sudo R -e 'install.packages("rmarkdown", repos="http://cran.us.r-project.org")'
+    sudo R -e 'install.packages("ggformula", repos="http://cran.us.r-project.org")'
+
+    #install RStudio
+    wget "https://download1.rstudio.org/rstudio-xenial-1.1.463-amd64.deb"
+    sudo apt install -y ./rstudio-xenial-1.1.463-amd64.deb
+    rm rstudio-xenial-1.1.463-amd64.deb
+
+    # qgis
+    echo "deb https://qgis.org/ubuntu $(upstream-lsb) main"  | sudo tee -a /etc/apt/sources.list
+    echo "deb-src https://qgis.org/ubuntu $(upstream-lsb) main"  | sudo tee -a /etc/apt/sources.list
+
+    # ownCloud sync
+
+    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_18.10/ /' > /etc/apt/sources.list.d/isv:ownCloud:desktop.list"
+    sudo apt-get update
+    sudo apt-get install owncloud-client
+
+fi
+
+#update
+sudo apt dist-upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+
+#clean
+sudo apt remove -y  pidgin
+sudo apt remove -y  hexchat
+sudo apt remove -y  rhythmbox
+sudo apt remove -y  mopidy
+sudo apt remove -y  xplayer
+sudo apt purge -y pidgin
+sudo apt purge -y hexchat
+sudo apt purge -y rhythmbox
+sudo apt purge -y mopidy
+sudo apt purge -y xplayer
+
+sudo apt autoremove -y
+sudo apt clean -y all
+rm -rf ~/.cache/thumbnails/*
+sudo du -sh /var/cache/apt
+
+#update
+sudo apt update -y
+sudo apt upgrade -y
