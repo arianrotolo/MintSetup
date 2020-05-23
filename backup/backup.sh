f8b195a9e9f5ffee27f9bb216d723e04a8cdbcff
KilianPolkov <34918355+KilianPolkov@users.noreply.github.com>
2020-05-23 12:44:09 -0300

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,18 +2,20 @@
 
 echo '*** Creando Copia ***'
 
+#crea carpetas y fichero de texto
 mkdir -p ~/Desktop/backup/home
+mkdir -p ~/Desktop/backup/dconf
 touch ~/Desktop/backup/home/list.txt
+touch ~/Desktop/backup/dconf/config.txt
 
+#crea copia archivos ~/ o HOME
 cp -v ~/.zshrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 cp -v ~/.bashrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
+cp -v ~/.profile ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 cp -v ~/.gitconfig ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 
-mkdir -p ~/Desktop/backup/dconf
-touch ~/Desktop/backup/dconf/used-config.txt
+#crea copia archivos org.cinnamon
 echo $'Path: /org/cinnamon/\nsounds\ntheme\ndesktop/keybindings/' > ~/Desktop/backup/dconf/used-config.txt
-
 dconf dump /org/cinnamon/ > ~/Desktop/backup/dconf/cinnamon-settings.txt
 
 echo '*** Copia Finalizada ***'
-
