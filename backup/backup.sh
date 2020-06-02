f707f3127885eabcd2d38c85fb855d8188fbf8aa
KilianPolkov <34918355+KilianPolkov@users.noreply.github.com>
2020-06-02 19:28:40 -0300

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,7 +7,7 @@ mkdir -p ~/Desktop/backup/dconf
 mkdir -p ~/Desktop/backup/home
 mkdir -p ~/Desktop/backup/home/Calibre\ Library/
 #crea ficheros de texto
-touch ~/Desktop/backup/dconf/cinnamon-settings.txt
+touch ~/Desktop/backup/dconf/config.txt
 touch ~/Desktop/backup/logs.txt
 
 #crea copia archivos ~/ o HOME
@@ -19,6 +19,6 @@ cp -v ~/Calibre\ Library/metadata.db ~/Desktop/backup/home/Calibre\ Library/ >> 
 
 #crea copia archivos org.cinnamon
 echo $'\nPaths: /org/cinnamon/\nsounds\ntheme\ndesktop/keybindings/' >> ~/Desktop/backup/logs.txt
-dconf dump /org/cinnamon/ > ~/Desktop/backup/dconf/cinnamon-settings.txt
+dconf dump /org/cinnamon/ > ~/Desktop/backup/dconf/config.txt
 
 echo '*** Copia Finalizada ***'
