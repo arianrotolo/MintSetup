a419fd34ff56506606f7ba22fc26ca7342ffe204
KilianPolkov <34918355+KilianPolkov@users.noreply.github.com>
2020-06-02 19:26:46 -0300

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,22 +2,23 @@
 
 echo '*** Creando Copia ***'
 
-#crea carpetas y fichero de texto
-mkdir -p ~/Desktop/backup/home
+#crea carpetas
 mkdir -p ~/Desktop/backup/dconf
-mkdir -p ~/Desktop/backup/Calibre\ Library/
-touch ~/Desktop/backup/home/list.txt
-touch ~/Desktop/backup/dconf/config.txt
+mkdir -p ~/Desktop/backup/home
+mkdir -p ~/Desktop/backup/home/Calibre\ Library/
+#crea ficheros de texto
+touch ~/Desktop/backup/dconf/cinnamon-settings.txt
+touch ~/Desktop/backup/logs.txt
 
 #crea copia archivos ~/ o HOME
-cp -v ~/.zshrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
-cp -v ~/.bashrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
-cp -v ~/.profile ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
-cp -v ~/.gitconfig ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
-cp -v ~/Calibre\ Library/metadata.db ~/Desktop/backup/home/Calibre\ Library/ >> ~/Desktop/backup/home/list.txt
+cp -v ~/.zshrc ~/Desktop/backup/home >> ~/Desktop/backup/logs.txt
+cp -v ~/.bashrc ~/Desktop/backup/home >> ~/Desktop/backup/logs.txt
+cp -v ~/.profile ~/Desktop/backup/home >> ~/Desktop/backup/logs.txt
+cp -v ~/.gitconfig ~/Desktop/backup/home >> ~/Desktop/backup/logs.txt
+cp -v ~/Calibre\ Library/metadata.db ~/Desktop/backup/home/Calibre\ Library/ >> ~/Desktop/backup/logs.txt
 
 #crea copia archivos org.cinnamon
-echo $'Path: /org/cinnamon/\nsounds\ntheme\ndesktop/keybindings/' > ~/Desktop/backup/dconf/used-config.txt
+echo $'\nPaths: /org/cinnamon/\nsounds\ntheme\ndesktop/keybindings/' >> ~/Desktop/backup/logs.txt
 dconf dump /org/cinnamon/ > ~/Desktop/backup/dconf/cinnamon-settings.txt
 
 echo '*** Copia Finalizada ***'
