379ccd894d695cdc024100c042fcf72f28aa4bb1
KilianPolkov <34918355+KilianPolkov@users.noreply.github.com>
2020-06-02 19:09:35 -0300

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,6 +5,7 @@ echo '*** Creando Copia ***'
 #crea carpetas y fichero de texto
 mkdir -p ~/Desktop/backup/home
 mkdir -p ~/Desktop/backup/dconf
+mkdir -p ~/Desktop/backup/Calibre Library/
 touch ~/Desktop/backup/home/list.txt
 touch ~/Desktop/backup/dconf/config.txt
 
@@ -13,7 +14,7 @@ cp -v ~/.zshrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 cp -v ~/.bashrc ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 cp -v ~/.profile ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
 cp -v ~/.gitconfig ~/Desktop/backup/home >> ~/Desktop/backup/home/list.txt
-cp -v ~/Calibre Library/metadata.db ~/Desktop/backup/home/Calibre Library >> ~/Desktop/backup/home/list.txt
+cp -v ~/Calibre Library/metadata.db ~/Desktop/backup/home/Calibre Library/ >> ~/Desktop/backup/home/list.txt
 
 #crea copia archivos org.cinnamon
 echo $'Path: /org/cinnamon/\nsounds\ntheme\ndesktop/keybindings/' > ~/Desktop/backup/dconf/used-config.txt
