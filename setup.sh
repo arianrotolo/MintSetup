97ed9b34c3fe11999dd4d3e04ab3feb3c317a49b
arianturing <49252428+arianturing@users.noreply.github.com>
2021-02-24 12:01:26 -0300

    Updated zsh option

--- setup.sh
+++ setup.sh
@@ -52,25 +52,23 @@ echo "Selected option for installation: $option!"
 alias version="grep UBUNTU_CODENAME /etc/os-release | grep -o --colour=never \"[a-z-]*$\""
 
 if [ $option == "zsh" ]; then
-    echo "Iniciando instalacion Oh My Zsh"
-
-    # install zsh shell
+    echo "Install zsh shell"
     sudo apt install -y zsh
     sudo chsh -s $(which zsh)
-    
-    # install Oh My Zsh (copiar .zshrc a /home)
+
+    echo "install Oh My Zsh (copiar .zshrc a /home)"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     
-    # install theme
+    echo "install theme"
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
     ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
     
-    # install plugins
+    echo "install plugins"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     
-    sudo apt update -y
-    echo "Instalacion finalizada. El equipo se reiniciara para terminar con la correcta instalacion."
+    update
+    echo "El equipo se reiniciara para finalizar la instalacion."
     sudo reboot
 fi
 
